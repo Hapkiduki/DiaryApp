82589face52587d21370dd8c5dd39a1f75547eb6
Hapkiduki <tenkan.af@gmail.com>
2023-02-10 17:11:52 -0500

    Initial commit

---
+++ app/src/main/java/com/hapkiduki/diaryapp/data/database/entity/ImageToUpload.kt
@@ -0,0 +1,15 @@
+package com.hapkiduki.diaryapp.data.database.entity
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.hapkiduki.diaryapp.util.Constants.IMAGE_TO_UPLOAD_TABLE
+
+@Entity(tableName = IMAGE_TO_UPLOAD_TABLE)
+data class ImageToUpload(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val remoteImagePath: String,
+    val imageUri: String,
+    val sessionUri: String
+)
---
+++ app/src/main/java/com/hapkiduki/diaryapp/di/DatabaseModule.kt
@@ -0,0 +1,43 @@
+package com.hapkiduki.diaryapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.hapkiduki.diaryapp.connectivity.NetworkConnectivityObserver
+import com.hapkiduki.diaryapp.data.database.ImagesDatabase
+import com.hapkiduki.diaryapp.util.Constants.IMAGES_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): ImagesDatabase {
+        return Room.databaseBuilder(
+            context = context,
+            klass = ImagesDatabase::class.java,
+            name = IMAGES_DATABASE
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirstDao(database: ImagesDatabase) = database.imageToUploadDao()
+
+    @Singleton
+    @Provides
+    fun provideSecondDao(database: ImagesDatabase) = database.imageToDeleteDao()
+
+    @Singleton
+    @Provides
+    fun provideNetworkConnectivityObserver(
+        @ApplicationContext context: Context
+    ) = NetworkConnectivityObserver(context = context)
+}
